8b4b2fcdfb852656503734362ef4d6254276e535
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2021-05-22 20:32:49 -0300

    add migration

---
+++ src/shared/infra/typeorm/migrations/1621725564433-Alter-table-audios-to-characters.ts
@@ -0,0 +1,32 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AlterTableAudiosToCharacters1621725564433 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+        await queryRunner.dropColumn('audios', 'dsc_audio');
+        await queryRunner.dropColumn('audios', 'audio_url');
+
+        await queryRunner.renameTable('audios', 'characters')
+        await queryRunner.addColumn('characters', new TableColumn({
+            name: 'name',
+            type:'varchar',
+            isNullable: true
+        }))
+        await queryRunner.addColumn('characters', new TableColumn({
+            name: 'avatar',
+            type:'varchar',
+            isNullable: true
+        }))
+        await queryRunner.addColumn('characters', new TableColumn({
+            name: 'voice',
+            type:'varchar',
+            isNullable: true
+        }))
+    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
